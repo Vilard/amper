57ea8cb0a9c9b17f9a5a45601ac1016aee580414
Vladimir <vladimir052290@mail.ru>
2018-07-09 14:19:13 +0300

    init

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.menu-icon').click(function() {
+    $('.trigger').slideToggle(500);
+  });//end slide toggle
+  
+ //  $(window).resize(function() {		
+	// 	if (  $(window).width() > 800 ) {			
+	// 		$('.trigger').removeAttr('style');
+	// 	 }
+	});//end resize
+	// $('')
